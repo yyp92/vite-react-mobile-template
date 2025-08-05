950d4b163afae96ed67321cf548e7f00ea367031
yyp92 <>
2025-08-05 15:09:50 +0800

    feat: 本地代理跨域配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,58 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import type {ConfigEnv} from 'vite'
 import path from 'path';
 import react from '@vitejs/plugin-react'
 import autoprefixer from 'autoprefixer';
 import postcsspxtoviewport from 'postcss-px-to-viewport';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base:'./',
+export default defineConfig(({mode}: ConfigEnv) => {
+    // 获取环境变量
+    const env = loadEnv(mode, process.cwd())
 
-  plugins: [
-    react(),
-  ],
+    return {
+        server: {
+            proxy: {
+                [env.VITE_APP_BASE_API]: {
+                    target: env.VITE_SERVE,
+                    changeOrigin: true,
+                    rewrite: (path) => path.replace(/^\/api/, '')
+                }
+            }
+        },
 
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-        additionalData: `@import "${path.resolve(__dirname, 'src/global.less')}";`
-      }
-    },
-    postcss: {
-      plugins: [
-        autoprefixer,
-        postcsspxtoviewport
-      ]
+        base:'./',
+
+        plugins: [
+            react(),
+        ],
+
+        css: {
+            preprocessorOptions: {
+                scss: {
+                    // 启用现代 API（关键配置）
+                    api: 'modern-compiler' 
+                },
+                sass: {
+                    // sass 和 scss 都需要配置
+                    api: 'modern-compiler' 
+                }
+            },
+            postcss: {
+                plugins: [
+                    autoprefixer,
+                    postcsspxtoviewport
+                ]
+            }
+        },
+
+        // ...其他配置项
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'src'),
+                '@components': path.resolve(__dirname, 'src/components'),
+                '@utils': path.resolve(__dirname, 'src/utils'),
+            }
+        }
     }
-  },
-
-  // ...其他配置项
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-      '@components': path.resolve(__dirname, 'src/components'),
-      '@utils': path.resolve(__dirname, 'src/utils'),
-    }
-  }
 })
